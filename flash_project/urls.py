fa2ebc1f56d27dc0ad072ef108a4882a032b9cd3
Pedro Haupt <115118401+Predox@users.noreply.github.com>
2025-12-12 16:58:38 -0300

    Implement media serving based on DJANGO_SERVE_MEDIA
    Add conditional media serving in production based on environment variable.

--- flash_project/urls.py
+++ flash_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.shortcuts import redirect
 from django.conf.urls.static import static
+import os
 
 
 def home_redirect(request):
@@ -20,3 +21,11 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Opcional: servir mídia em produção sem CDN, controlado por env
+    if os.getenv("DJANGO_SERVE_MEDIA", "False") == "True":
+        from django.views.static import serve
+        from django.urls import re_path
+        urlpatterns += [
+            re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+        ]
